f90d240ff341b6109c755bb4e48adecb71be9b0d
Daniele Trovato <plasticsoup@yahoo.co.uk>
2022-08-17 21:17:28 +0100

    Add html for main page and for add contact form. Add css to style html and add javascript to attach event listeners to add contact buttons which toggle the placeholder main page and the form. Also add event listener to cancel button to toggle back form and main page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,38 @@
-console.log(9);
+function showForm() {
+  const addContactsButtons = [...document.querySelectorAll(".add-contact")];
+  addContactsButtons.forEach(button => {
+    button.addEventListener("click", event => {
+      toggleForm();
+    });
+  });
+}
+
+function toggleForm() {
+  const $form = $("#form-container");
+  const $main = $("main");
+  $form.slideToggle();
+  $main.slideToggle();
+}
+
+function cancelCreateContact() {
+  const $cancel = $("#cancel-create-contact");
+  $cancel.click(() => toggleForm());
+}
+
+function submitForm() {
+  const $submit = $("#submit");
+  $submit.click(event => {
+    event.preventDefault();
+    console.log("fava");
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  // attach click event listeners to the add contact buttons to show form
+  showForm();
+  // attach click event listeners to the add contact buttons to restore main page
+  cancelCreateContact();
+  // attach click event listener to submit button in the form
+  submitForm();
+});
